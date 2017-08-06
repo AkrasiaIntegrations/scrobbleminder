a89a95e0a015750be858c85c84586407fd71d871
System <none>
2017-08-06 15:10:12 +0000

    👘💳 Updated with Glitch

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ var akrasiaCallback = function(session, result) {
   })
 };
 
+// The API key from the .env file secrets
+var apiKey = process.env.LAST_FM_API_KEY;
+
 app.post("/fetch", function(request, response) {
   console.log('Fetch called.');
   // Get the session and user options from Akrasia's request
@@ -30,18 +33,18 @@ app.post("/fetch", function(request, response) {
   response.send({'result': 'started'});
   
   // Get the user's forum name (set and stored through Akrasia) from the passed options
-  var forumUsername = userOptions.forum_username;
+  var username = userOptions.lastfm_username;
   
   // Get the user's profile from the forum API, grab the post count, and submit the datapoint to Akrasia
-  Request('http://forum.beeminder.com/u/' +  forumUsername + '/summary.json', function(err, res, body) {
-    var forumData = JSON.parse(body);
+  Request('http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=' + username + '&api_key=' + apiKey + '&format=json', function(err, res, body) {
+    var apiData = JSON.parse(body);
     
-    var forumPostCount = forumData.user_summary.post_count;
+    var playCount = apiData.user.playcount;
     
     // Create the datapoint with value (and optional comment)
     var result = {
-      value: forumPostCount,
-      comment: 'via Akrasia integration: Beeminder Forum Posts Count'
+      value: playCount,
+      comment: 'via Akrasia integration: Last.fm Play Count'
     };
     
     // Send the datapoint to Akrasia
